45abb1b81b79dd904a3dd8a183c4fe5105be939c
Hato1125 <108814993+Hato1125@users.noreply.github.com>
2024-09-05 22:59:59 +0900

    Update: Hyprland.confの更新

---
+++ dotfiles/ags/App/Desktop.ts
@@ -0,0 +1,58 @@
+import Gtk from 'gi://Gtk';
+import App from 'resource:///com/github/Aylur/ags/app.js';
+
+import { Wallpaper } from '@Lib/Wallpaper';
+
+import Hyprland from "@Service/Hyprland";
+
+import {
+  ScreenCorners,
+  StatusbarCorners,
+} from '@App/Desktop/Corner/Corner';
+
+import Statusbar from '@App/Desktop/Statusbar/Statusbar';
+import PowerAction from '@App/Desktop/PowerAction/PowerAction';
+
+type desktopWindowType = (monitor: number) => Gtk.Window;
+const DESKTOP_WINDOW: desktopWindowType[] = [
+  ...ScreenCorners,
+  ...StatusbarCorners,
+  Statusbar,
+];
+
+type popupWindowType = () => Gtk.Window;
+const POPUP_WINDOW: popupWindowType[] = [
+  PowerAction,
+];
+
+function addMonitorDesktopWindows(monitor: number): void {
+  for (const window of DESKTOP_WINDOW)
+    App.addWindow(window(monitor));
+}
+
+function addMonitorPopupWindows(): void {
+  for (const window of POPUP_WINDOW)
+    App.addWindow(window());
+}
+
+function MonitorAdded(disp: object, monitor: number): void {
+  addMonitorDesktopWindows(monitor);
+}
+
+function MonitorRemoved(disp: object, monitor: number): void {
+  for (const window of App.windows) {
+    if (window.gdkmonitor === monitor)
+      App.removeWindow(window);
+  }
+}
+
+export default (): void => {
+  Wallpaper.SetWallpaper(Wallpaper.GetWallpaper());
+
+  Hyprland.connect('monitor-added', MonitorAdded);
+  Hyprland.connect('monitor-removed', MonitorRemoved);
+
+  for (const monitor of Hyprland.monitors)
+    addMonitorDesktopWindows(monitor.id);
+  addMonitorPopupWindows();
+}
